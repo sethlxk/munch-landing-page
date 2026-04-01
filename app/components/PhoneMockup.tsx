bdcaee1edae180e93a919430a2415f31c6dea4da
sethlxk <sethlxk@gmail.com>
2026-04-01 12:06:16 +0800

    use new phone mockup image

--- app/components/PhoneMockup.tsx
+++ app/components/PhoneMockup.tsx
@@ -9,12 +9,13 @@ export default function PhoneMockup() {
       <div className="absolute inset-0 bg-munch-dark/10 rounded-[3rem] blur-2xl scale-105 translate-y-4" />
 
       {/* Phone frame */}
-      <div className="relative w-[260px] sm:w-[300px] h-[540px] sm:h-[620px] bg-munch-dark rounded-[3rem] p-3 shadow-2xl">
+      <div className="relative w-[260px] sm:w-[300px] h-[540px] sm:h-[570px] bg-munch-dark rounded-[3rem] p-3 shadow-2xl">
         {/* Screen with imported image */}
         <div className="w-full h-full rounded-[2.4rem] overflow-hidden relative bg-white">
           <Image
-            src="/app-screenshot.png"
+            src="/home.png"
             alt="Munch app screenshot"
+            style={{ objectFit: "contain" }}
             fill
             className="object-cover object-top"
             priority
